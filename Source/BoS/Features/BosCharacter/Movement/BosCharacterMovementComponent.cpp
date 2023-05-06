bff57eb39db420dc5f81086845841c13f46bc3b3
Valeriy <v.borovkov@madlab.ltd>
2023-05-06 17:36:04 +0300

    CanBeCompiled

--- Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.cpp
+++ Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "BosCharacterMovementComponent.h"
 
+#include "GameFramework/Character.h"
+
 
 void FSavedMove_Bos::Clear()
 {
@@ -12,7 +14,7 @@ void FSavedMove_Bos::Clear()
 
 uint8 FSavedMove_Bos::GetCompressedFlags() const
 {
-	bool Result = Super::GetCompressedFlags();
+	uint8 Result = Super::GetCompressedFlags();
 	/*
 	FLAG_JumpPressed	= 0x01,	// Jump pressed
 	FLAG_WantsToCrouch	= 0x02,	// Wants to crouch
@@ -20,7 +22,7 @@ uint8 FSavedMove_Bos::GetCompressedFlags() const
 	FLAG_Reserved_2		= 0x08,	// Reserved for future use
 	// Remaining bit masks are available for custom flags.
 	FLAG_Custom_0		= 0x10, // IsSprinting
-	FLAG_Custom_1		= 0x20,
+	FLAG_Custom_1		= 0x20, // IsCrouch
 	FLAG_Custom_2		= 0x40,
 	FLAG_Custom_3		= 0x80,
 	*/
@@ -41,11 +43,27 @@ void FSavedMove_Bos::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const&
 	FNetworkPredictionData_Client_Character& ClientData)
 {
 	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
+	UBosCharacterMovementComponent* MovementComponent = StaticCast<UBosCharacterMovementComponent*>(C->GetMovementComponent());
+	bSaveIsSprinting = MovementComponent->HasSprint;
 }
 
 void FSavedMove_Bos::PrepMoveFor(ACharacter* C)
 {
 	FSavedMove_Character::PrepMoveFor(C);
+	UBosCharacterMovementComponent* MovementComponent = StaticCast<UBosCharacterMovementComponent*>(C->GetMovementComponent());
+	MovementComponent->HasSprint = bSaveIsSprinting;
+}
+
+
+FNetworkPredictionData_Client_Character_Bos::FNetworkPredictionData_Client_Character_Bos(
+	const UCharacterMovementComponent& ClientComponent)
+		: Super(ClientComponent)
+{
+}
+
+FSavedMovePtr FNetworkPredictionData_Client_Character_Bos::AllocateNewMove()
+{
+	return FNetworkPredictionData_Client_Character::AllocateNewMove();
 }
 
 // Sets default values for this component's properties
@@ -59,6 +77,35 @@ float UBosCharacterMovementComponent::GetMaxSpeed() const
 	if (HasSprint)
 		result = SprintSpeed;
 	return result;
+}
+
+FNetworkPredictionData_Client* UBosCharacterMovementComponent::GetPredictionData_Client() const
+{
+	// Немножечко черной магии
+	if(ClientPredictionData == nullptr)
+	{
+		UBosCharacterMovementComponent* MutableThis = const_cast<UBosCharacterMovementComponent*>(this);
+		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_Character_Bos(*this);
+	}
+	return ClientPredictionData;
+}
+
+void UBosCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+	/*
+	FLAG_JumpPressed	= 0x01,	// Jump pressed
+	FLAG_WantsToCrouch	= 0x02,	// Wants to crouch
+	FLAG_Reserved_1		= 0x04,	// Reserved for future use
+	FLAG_Reserved_2		= 0x08,	// Reserved for future use
+	// Remaining bit masks are available for custom flags.
+	FLAG_Custom_0		= 0x10, // IsSprinting
+	FLAG_Custom_1		= 0x20, // IsCrouch
+	FLAG_Custom_2		= 0x40,
+	FLAG_Custom_3		= 0x80,
+	*/
+	
+	HasSprint = (Flags &= FSavedMove_Character::FLAG_Custom_0) != 0;
 }
 
 void UBosCharacterMovementComponent::StartSprinting()
