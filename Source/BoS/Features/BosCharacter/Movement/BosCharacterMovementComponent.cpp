1f5cb135e8c0e8e309a49d9c42b06fed9dec380d
Valeriy <v.borovkov@madlab.ltd>
2023-05-07 15:18:38 +0300

    Revert "CanBeCompiled"
    This reverts commit bff57eb39db420dc5f81086845841c13f46bc3b3.

--- Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.cpp
+++ Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.cpp
@@ -3,8 +3,6 @@
 
 #include "BosCharacterMovementComponent.h"
 
-#include "GameFramework/Character.h"
-
 
 void FSavedMove_Bos::Clear()
 {
@@ -14,7 +12,7 @@ void FSavedMove_Bos::Clear()
 
 uint8 FSavedMove_Bos::GetCompressedFlags() const
 {
-	uint8 Result = Super::GetCompressedFlags();
+	bool Result = Super::GetCompressedFlags();
 	/*
 	FLAG_JumpPressed	= 0x01,	// Jump pressed
 	FLAG_WantsToCrouch	= 0x02,	// Wants to crouch
@@ -22,7 +20,7 @@ uint8 FSavedMove_Bos::GetCompressedFlags() const
 	FLAG_Reserved_2		= 0x08,	// Reserved for future use
 	// Remaining bit masks are available for custom flags.
 	FLAG_Custom_0		= 0x10, // IsSprinting
-	FLAG_Custom_1		= 0x20, // IsCrouch
+	FLAG_Custom_1		= 0x20,
 	FLAG_Custom_2		= 0x40,
 	FLAG_Custom_3		= 0x80,
 	*/
@@ -43,27 +41,11 @@ void FSavedMove_Bos::SetMoveFor(ACharacter* C, float InDeltaTime, FVector const&
 	FNetworkPredictionData_Client_Character& ClientData)
 {
 	FSavedMove_Character::SetMoveFor(C, InDeltaTime, NewAccel, ClientData);
-	UBosCharacterMovementComponent* MovementComponent = StaticCast<UBosCharacterMovementComponent*>(C->GetMovementComponent());
-	bSaveIsSprinting = MovementComponent->HasSprint;
 }
 
 void FSavedMove_Bos::PrepMoveFor(ACharacter* C)
 {
 	FSavedMove_Character::PrepMoveFor(C);
-	UBosCharacterMovementComponent* MovementComponent = StaticCast<UBosCharacterMovementComponent*>(C->GetMovementComponent());
-	MovementComponent->HasSprint = bSaveIsSprinting;
-}
-
-
-FNetworkPredictionData_Client_Character_Bos::FNetworkPredictionData_Client_Character_Bos(
-	const UCharacterMovementComponent& ClientComponent)
-		: Super(ClientComponent)
-{
-}
-
-FSavedMovePtr FNetworkPredictionData_Client_Character_Bos::AllocateNewMove()
-{
-	return FNetworkPredictionData_Client_Character::AllocateNewMove();
 }
 
 // Sets default values for this component's properties
@@ -77,35 +59,6 @@ float UBosCharacterMovementComponent::GetMaxSpeed() const
 	if (HasSprint)
 		result = SprintSpeed;
 	return result;
-}
-
-FNetworkPredictionData_Client* UBosCharacterMovementComponent::GetPredictionData_Client() const
-{
-	// Немножечко черной магии
-	if(ClientPredictionData == nullptr)
-	{
-		UBosCharacterMovementComponent* MutableThis = const_cast<UBosCharacterMovementComponent*>(this);
-		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_Character_Bos(*this);
-	}
-	return ClientPredictionData;
-}
-
-void UBosCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
-{
-	Super::UpdateFromCompressedFlags(Flags);
-	/*
-	FLAG_JumpPressed	= 0x01,	// Jump pressed
-	FLAG_WantsToCrouch	= 0x02,	// Wants to crouch
-	FLAG_Reserved_1		= 0x04,	// Reserved for future use
-	FLAG_Reserved_2		= 0x08,	// Reserved for future use
-	// Remaining bit masks are available for custom flags.
-	FLAG_Custom_0		= 0x10, // IsSprinting
-	FLAG_Custom_1		= 0x20, // IsCrouch
-	FLAG_Custom_2		= 0x40,
-	FLAG_Custom_3		= 0x80,
-	*/
-	
-	HasSprint = (Flags &= FSavedMove_Character::FLAG_Custom_0) != 0;
 }
 
 void UBosCharacterMovementComponent::StartSprinting()
--- Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.h
+++ Source/BoS/Features/BosCharacter/Movement/BosCharacterMovementComponent.h
@@ -7,17 +7,26 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "BosCharacterMovementComponent.generated.h"
 
+class FSavedMove_Bos : FSavedMove_Character
+{
+	typedef FSavedMove_Character Super;
+public:
+	virtual void Clear() override;
+	virtual uint8 GetCompressedFlags() const override;
+	virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+	virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+	virtual void PrepMoveFor(ACharacter* C) override;
+
+private:
+	uint8 bSaveIsSprinting : 1;
+};
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BOS_API UBosCharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
-	
-	friend class FSavedMove_Bos;
 
 public:
 
-	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
-	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
 	FORCEINLINE bool IsSprinting() { return HasSprint; };
 	UBosCharacterMovementComponent();
 	virtual float GetMaxSpeed() const override;
@@ -32,28 +41,4 @@ protected:
 private:
 	bool HasSprint;
 	void ChangeMaxWalkSpeed(float InNewMaxWalkSpeed);
-};
-
-class FSavedMove_Bos : FSavedMove_Character
-{
-	typedef FSavedMove_Character Super;
-public:
-	virtual void Clear() override;
-	virtual uint8 GetCompressedFlags() const override;
-	virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
-	virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
-	virtual void PrepMoveFor(ACharacter* C) override;
-
-private:
-	uint8 bSaveIsSprinting : 1;
-};
-
-class FNetworkPredictionData_Client_Character_Bos : public FNetworkPredictionData_Client_Character
-{
-	typedef FNetworkPredictionData_Client_Character Super;
-	
-public:
-	FNetworkPredictionData_Client_Character_Bos(const UCharacterMovementComponent& ClientComponent);
-
-	virtual FSavedMovePtr AllocateNewMove() override;
 };
